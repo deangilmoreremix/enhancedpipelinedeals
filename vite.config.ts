2bb667517e5313bc5792d7bd5bd9226bff47e886
Dean Gilmore <dean@videoremix.io>
2025-09-10 10:33:23 +0000

    Update vite.config.ts with module federation configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  plugins: [
+    react(),
+    federation({
+      name: "deals",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App.tsx",
+      },
+      shared: {
+        react: { singleton: true, eager: true, requiredVersion: "^18.0.0" },
+        "react-dom": { singleton: true, eager: true, requiredVersion: "^18.0.0" },
+        // "react-router-dom": { singleton: true, eager: true },
+      },
+    }),
+  ],
+  build: { target: "esnext", modulePreload: false, cssCodeSplit: true },
+  define: { global: "globalThis" },
+  server: { host: true, port: 5176 },
 });
